c4d73edab2d57882e8d003218d6def69c7bd0aac
Dion <hallo@ikbendion.nl>
2021-05-31 17:06:44 +0100

    added integ in getpin func

--- endpoint/endpoint.py
+++ endpoint/endpoint.py
@@ -47,7 +47,7 @@ class getpin(Resource):
     def get():
         pin = request.args.get('pin')
         # pi code #
-        state = GPIO.input(pin)
+        state = GPIO.input(int(pin))
         payload = str(pin)+' = '+str(state)
         return payload
 
